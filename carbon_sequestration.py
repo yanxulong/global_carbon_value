cdf4741390d745405dd5f4ee9226279ca2f006ea
long <long0555@umn.edu>
2025-04-01 16:12:08 -0500

    Update carbon_sequestration.py

--- carbon_sequestration.py
+++ carbon_sequestration.py
@@ -1,1 +1,13 @@
+import os
+
+
+base_data_dir = "/Users/long/Library/CloudStorage/GoogleDrive-yxlong@umn.edu/Shared drives/NatCapTEEMs/Files/base_data/submissions/carbon_density/data"
+data_filename_above_ground = "aboveground_biomass_carbon_2010.tif"
+data_filename_above_ground_path = os.path.join(base_data_dir, data_filename_above_ground) 
+
+
+
+
+
+
 
